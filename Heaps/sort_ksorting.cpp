aa01d2739990a9d05e6650fafbfac26946538f50
Aman <amansinghh8116@gmail.com>
2024-11-30 22:34:35 +0530

    content added in last file

---
+++ Heaps/sort_ksorting.cpp
@@ -0,0 +1,20 @@
+int main()
+{
+    priority_queue<int, vector<int>, greater<int>> minh;
+    vector<int> v;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        minh.push(arr[i]);
+        if (minh.size() > k)
+        {
+            v.push_back(minh.top());
+            minh.top();
+        }
+    }
+    while (minh.size() > 0)
+    {
+        v.push_back(minh.top());
+        minh.pop();
+    }
+    return v;
+}
