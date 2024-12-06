c0d31bcf26aea08edccb17298366bffddea52447
Aman <amansinghh8116@gmail.com>
2024-12-06 14:36:29 +0530

    new file added

---
+++ Heaps/top_k_frequent.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    vector<int> topKFrequent(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> mp;
+        vector<int> v;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minh;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            mp[nums[i]]++;
+        }
+        for (auto it : mp)
+        {
+            minh.push({it.second, it.first});
+            if (minh.size() > k)
+            {
+                minh.pop();
+            }
+        }
+        while (minh.size() > 0)
+        {
+            v.push_back(minh.top().second);
+            minh.pop();
+        }
+        return v;
+    }
+};
